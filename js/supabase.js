b228c6acaf09bade56d3aa68f9ead2b1e50fbbbd
Sitenova studio <skaikhsahil406@gmail.com>
2026-06-15 22:02:30 +0530

    Create supabase.js

---
+++ js/supabase.js
@@ -0,0 +1,8 @@
+const SUPABASE_URL = "https://irsdulqoedvfthsdqoob.supabase.co/rest/v1/";
+
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imlyc2R1bHFvZWR2ZnRoc2Rxb29iIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODE1MjQ2OTYsImV4cCI6MjA5NzEwMDY5Nn0.6ci19F8osVGEhpNcCq1xb_1da7WObaLVfOT3wT32Eps";
+
+const supabaseClient = supabase.createClient(
+    SUPABASE_URL,
+    SUPABASE_ANON_KEY
+);
