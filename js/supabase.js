819906bf8ec8779d4561edbe5699bef150eb8023
Sitenova studio <skaikhsahil406@gmail.com>
2026-06-15 22:10:38 +0530

    Update supabase.js

--- js/supabase.js
+++ js/supabase.js
@@ -1,4 +1,4 @@
-const SUPABASE_URL = "https://irsdulqoedvfthsdqoob.supabase.co/rest/v1/";
+const SUPABASE_URL = "https://irsdulqoedvfthsdqoob.supabase.co";
 
 const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imlyc2R1bHFvZWR2ZnRoc2Rxb29iIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODE1MjQ2OTYsImV4cCI6MjA5NzEwMDY5Nn0.6ci19F8osVGEhpNcCq1xb_1da7WObaLVfOT3wT32Eps";
 
